11e0571d3f0311c91c3ea5cde66506f57de46ee0
BushraFatimaBF <102679041+BushraFatimaBF@users.noreply.github.com>
2023-06-11 20:17:26 +0100

    categories work now

--- news-around-app/src/Components/NewsArticle.js
+++ news-around-app/src/Components/NewsArticle.js
@@ -54,54 +54,7 @@ function NewsArticle({allArticles, categoryArticles, getCategoryHeadlines}){
         
 export default NewsArticle
 
-// <Link to={url}>
-
-
-/**
-return(<div>
-    {allArticles.map(({title, author, source})=>(
-        <VStack spacing={20} direction='row' >
-        <Card w='400px' h='400px' key={title}>
-        <CardBody>
-        <Image ></Image>
-        <Text color="#0050C8" >{title}</Text>
-        <Text>{author}</Text>
-        <Text>{source}</Text>
-        </CardBody>
-        </Card>
-        </VStack>
-    ))}
-       
-    </div>
-    )
- */
 
 
 
 
-
-
-
-
-
-
-
-// src="https://media.discordapp.net/attachments/1101934137029435533/1113442514104483840/What_Happens_logo.png?width=936&height=936"
-
-/**
- * <div>
-    {searchedData.slice(0,10).map(({ id, name, house, image, ancestry, patronus}) => (
-      <VStack spacing={10} direction='row'>
-      <Card w='400px' h='400px'>
-        <CardBody key={id}>
-          <Text fontSize='2xl'>{name}</Text>
-          <Text fontSize='l'>House: {house}</Text>
-          <Text fontSize='l'>Ancestry: {ancestry}</Text>
-          <Text fontSize='l'>Patronous: {patronus}</Text>
-          <Image src={image} alt={name}  boxSize='200px'/>
-        </CardBody>
-        </Card>
-        </VStack>
-      ))}
-        </div>
- */
